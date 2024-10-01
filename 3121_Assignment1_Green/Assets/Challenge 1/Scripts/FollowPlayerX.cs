46c71000573fe38a8723e580519ff7c8473e2b83
Samantha Green <115169809+elentiyafireheart@users.noreply.github.com>
2024-10-01 12:06:42 -0400

    Camera moves with Player

---
+++ 3121_Assignment1_Green/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(50, -3, 0);
+        transform.rotation = Quaternion.Euler(0, -90, 0);
+        offset = transform.position - plane.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(plane.transform.position.x + offset.x,
+            plane.transform.position.y + offset.y,
+            plane.transform.position.z + offset.x);
+    }
+}
